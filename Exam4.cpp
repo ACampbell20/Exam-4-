7afb2f9276f6250f0a9f46968cf0b2c805e715fe
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-08-03 11:23:38 -0500

    Add main function to Exam4.cpp with employee demo

---
+++ Exam4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#include "Developer.h"
+#include "Manager.h"
+#include "Employee.h"
+
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+
+    Developer developer;
+    Manager manager("Engineering");
+
+    Employee emp1("Alice", &developer);
+    Employee emp2("Bob", &manager);
+
+    emp1.work();
+    emp2.work();
+
+    return 0;
+}
